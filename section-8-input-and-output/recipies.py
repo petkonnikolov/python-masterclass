0e4764ee18a5ff34ce2a4e8dc725074854754128
petkonnikolov <petko18@yahoo.com>
2021-04-20 15:10:27 +0300

    updates

---
+++ section-8-input-and-output/recipies.py
@@ -0,0 +1,19 @@
+import shelve
+
+blt = ["bacon", "lettuce", "tomato", "bread"]
+beans_on_toast = ["beans", "bread"]
+scrambled_eggs = ["eggs", "butter", "milk"]
+soup = ["tin of soup"]
+pasta = ["pasta", "cheese"]
+
+with shelve.open('recipes', writeback=True) as recipes:
+    # recipes["blt"] = blt
+    # recipes["beans on toast"] = beans_on_toast
+    # recipes["scrambled eggs"] = scrambled_eggs
+    # recipes["pasta" ] = pasta
+    # recipes["soup"] = soup
+
+    # recipes['soup'].append('croutons')
+
+    for snack in recipes:
+        print(snack, recipes[snack])
