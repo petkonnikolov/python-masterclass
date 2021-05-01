39d0a161523b5bc4eed38d13d0074e117eeb9fe0
petkonnikolov <petko18@yahoo.com>
2021-05-01 15:30:30 +0300

    update

--- section-9-modules-and-functions/tkinterdemo.py
+++ section-9-modules-and-functions/tkinterdemo.py
@@ -11,7 +11,19 @@ mainWindow.geometry("640x480+8+400")
 label = tkinter.Label(mainWindow, text="Hello World")
 label.pack(side='top')
 
-canvas = tkinter.Canvas(mainWindow, relief='raised', borderwidth=1)
-canvas.pack(side='left')
+leftFrame = tkinter.Frame(mainWindow)
+leftFrame.pack(side='left', anchor='n', fill=tkinter.Y, expand=False)
+canvas = tkinter.Canvas(leftFrame, relief='raised', borderwidth=1)
+canvas.pack(side='left', anchor='n')
+
+rightFrame = tkinter.Frame(mainWindow)
+rightFrame.pack(side='left', anchor='n', expand='true')
+
+button1 = tkinter.Button(rightFrame, text="Button 1")
+button2 = tkinter.Button(rightFrame, text="Button 2")
+button3 = tkinter.Button(rightFrame, text="Button 3")
+button1.pack(side='top')
+button2.pack(side='top')
+button3.pack(side='top')
 
 mainWindow.mainloop()
