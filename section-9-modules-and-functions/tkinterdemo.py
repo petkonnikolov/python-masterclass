2a908197f9b487d34d0f1bc7a7f47f10a53f3ec9
petkonnikolov <petko18@yahoo.com>
2021-05-02 12:11:43 +0300

    updates

--- section-9-modules-and-functions/tkinterdemo.py
+++ section-9-modules-and-functions/tkinterdemo.py
@@ -6,24 +6,37 @@ except ImportError: # python 2
 mainWindow = tkinter.Tk()
 
 mainWindow.title("Hello World")
-mainWindow.geometry("640x480+8+400")
+mainWindow.geometry("640x480-8-200")
 
 label = tkinter.Label(mainWindow, text="Hello World")
-label.pack(side='top')
+label.grid(row=0, column=0)
 
 leftFrame = tkinter.Frame(mainWindow)
-leftFrame.pack(side='left', anchor='n', fill=tkinter.Y, expand=False)
+leftFrame.grid(row=1, column=1)
 canvas = tkinter.Canvas(leftFrame, relief='raised', borderwidth=1)
-canvas.pack(side='left', anchor='n')
+canvas.grid(row=1, column=0)
 
 rightFrame = tkinter.Frame(mainWindow)
-rightFrame.pack(side='left', anchor='n', expand='true')
+rightFrame.grid(row=1, column=2)
 
 button1 = tkinter.Button(rightFrame, text="Button 1")
 button2 = tkinter.Button(rightFrame, text="Button 2")
 button3 = tkinter.Button(rightFrame, text="Button 3")
-button1.pack(side='top')
-button2.pack(side='top')
-button3.pack(side='top')
+button1.grid(row=0, column=0)
+button2.grid(row=1, column=0)
+button3.grid(row=2, column=0)
+
+# configure the columns
+mainWindow.columnconfigure(0, weight=1)
+mainWindow.columnconfigure(1, weight=1)
+mainWindow.grid_columnconfigure(2, weight=1)
+
+leftFrame.config(relief='sunken', borderwidth=1)
+rightFrame.config(relief='sunken', borderwidth=1)
+leftFrame.grid(sticky='ns')
+rightFrame.grid(sticky='new')
+
+rightFrame.columnconfigure(0, weight=1)
+button2.grid(sticky='ew')
 
 mainWindow.mainloop()
