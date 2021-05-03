a52634003b8b2726c56278b70f76478061dceb2d
petkonnikolov <petko18@yahoo.com>
2021-05-03 16:11:36 +0300

    update

--- section-9-modules-and-functions/functions.py
+++ section-9-modules-and-functions/functions.py
@@ -5,13 +5,17 @@ def python_food():
     print(" " * left_margin, text)
 
 
-def centre_text(text):
-    text = str(text)
+def centre_text(*args, sep=' ', end='\n', file=None, flush=False):
+    text = ""
+    for arg in args:
+        text += str(arg) + sep
     left_margin = (80 - len(text)) // 2
-    print(" " * left_margin, text)
+    print(" " * left_margin, text, end=end, file=file, flush=flush)
 
 # call the function
 centre_text("spam and eggs")
 centre_text("spam, spam and eggs")
 centre_text(12)
 centre_text("spam, spam, spam and spam")
+
+centre_text("first", "second", 1, 2, "spam", sep=':')
